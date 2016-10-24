9f5b7e4fad2de12ea69b3d9eee24407888f3dbe3
Justin <justindado@yahoo.com>
2016-10-24 19:33:11 +1300

    trying and failing to render index on '/'

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import exphbs from 'express-handlebars'
-import graphql from 'graphql'
+import {graphql} from 'graphql'
 import graphqlHTTP from 'express-graphql'
-import {buildSchema} from 'graphql'
 
-import MySchema from './src/schema.js'
+import Schema from './src/schema.js'
 
 const app = express();
 
@@ -12,9 +11,21 @@ app.engine('hbs', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'hbs')
 app.set('views', `${__dirname}views`)
 app.use('/graphiql', graphqlHTTP({
-  schema: MySchema,
+  schema: Schema,
   graphiql: process.env.NODE_ENV || 'development'
 }))
+
+app.get('/', () => {
+  graphql(Schema, `
+    {
+      heroes {
+        _id
+        name
+        affiliation
+      }
+    }`)
+    .then(res => res.render('index', {data: data}))
+})
 
 app.listen(1337);
 console.log('GraphiQL interface on: 1337/graphiql');
