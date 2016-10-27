0d7345a621b6f7029254a8129e6dac686683dda2
Justin <justindado@yahoo.com>
2016-10-27 17:11:14 +1300

    removed unnecessary semicolons

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import graphqlHTTP from 'express-graphql'
 
 import Schema from './src/schema.js'
 
-const app = express();
+const app = express()
 
 app.engine('hbs', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'hbs')
@@ -27,5 +27,5 @@ app.get('/', () => {
     .then(res => res.render('index', {data: data}))
 })
 
-app.listen(1337);
-console.log('GraphiQL interface on: 1337/graphiql');
+app.listen(1337)
+console.log('GraphiQL interface on: 1337/graphiql')
